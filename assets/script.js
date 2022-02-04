496d7145b1e4448f0569fdfe3a9b9124e1598a94
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-02-04 15:04:58 -0300

    Versão 0.2 - Implementada distribuição aleatória de cartas

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+//Escolha de quantidade de cartas
+
 let qtdCartas = 0;
 let count = 0;
 
@@ -5,8 +7,32 @@ while(qtdCartas > 14 || qtdCartas < 4 || qtdCartas%2 != 0) {
     qtdCartas = parseInt(prompt("Quantas cartas você deseja? (números pares de 4 a 14)"));
 }
 
+
+//Preenchimento da array
+
+let cartaFrente = [];
+
+for(let i = 0; i < qtdCartas; i++){
+    cartaFrente[i] = (i+1);
+}
+
+//Embaralhamento da array
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+cartaFrente.sort(comparador);
+console.log(cartaFrente);
+
+//Adicionando cartas na mesa
+
 for(count = 0; count < qtdCartas; count++) {
     //console.log(count);
     const cards = document.querySelector(".cartas");
-    cards.innerHTML +=`<div class="umaCarta"></div>`
+    cards.innerHTML +=`
+    <div class="umaCarta">
+        <img src="/gifs and images/front.png" alt="Papagaio">
+        <p>${cartaFrente[count]}</p>
+    </div>
+    `
 }
