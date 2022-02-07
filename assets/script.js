169d4a4765e06c612604fc1908536f5aa24af662
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-02-07 11:26:31 -0300

    Versão 0.2.1 - Implementado o giro das cartas e o gif na parte frontal

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+let arrayDeGifs = ["<img src='/gifs and images/metalparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/metalparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/bobrossparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/bobrossparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/explodyparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/explodyparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/fiestaparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/fiestaparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/revertitparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/revertitparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/tripletsparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/tripletsparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/unicornparrot.gif' alt='Papagaio'>", 
+"<img src='/gifs and images/unicornparrot.gif' alt='Papagaio'>", ];
+
 //Escolha de quantidade de cartas
 
 let qtdCartas = 0;
@@ -13,7 +28,7 @@ while(qtdCartas > 14 || qtdCartas < 4 || qtdCartas%2 != 0) {
 let cartaFrente = [];
 
 for(let i = 0; i < qtdCartas; i++){
-    cartaFrente[i] = (i+1);
+    cartaFrente[i] = i;
 }
 
 //Embaralhamento da array
@@ -31,8 +46,43 @@ for(count = 0; count < qtdCartas; count++) {
     const cards = document.querySelector(".cartas");
     cards.innerHTML +=`
     <div class="umaCarta">
-        <img src="/gifs and images/front.png" alt="Papagaio">
-        <p>${cartaFrente[count]}</p>
+        <div class="face front" onclick="girarCartaFront(this)">
+            ${arrayDeGifs[cartaFrente[count]]}
+        </div>
+
+        <div class="face back" onclick="girarCartaBack(this)">
+            <img src="/gifs and images/front.png" alt="Papagaio">
+        </div>
     </div>
     `
+}
+
+function girarCartaFront(carta) {
+    //const lado = document.querySelector(carta);
+    carta.classList.add("front-face");
+    if(carta.classList.contains("front-face-return")) {
+        carta.classList.remove("front-face-return");
+    }
+    setTimeout(retornarCartaFront, 3000);
+}
+
+function girarCartaBack(carta) {
+    //const lado = document.querySelector(carta);
+    carta.classList.add("back-face");
+    if(carta.classList.contains("back-face-return")) {
+        carta.classList.remove("back-face-return");
+    }
+    setTimeout(retornarCartaBack, 3000);
+}
+
+function retornarCartaFront() {
+    const carta = document.querySelector(".front-face");
+    carta.classList.add("front-face-return");
+    carta.classList.remove("front-face");
+}
+
+function retornarCartaBack() {
+    const carta = document.querySelector(".back-face");
+    carta.classList.add("back-face-return");
+    carta.classList.remove("back-face");
 }
